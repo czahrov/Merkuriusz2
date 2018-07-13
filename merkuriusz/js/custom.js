c130be9c33d78b49e1cf6ebad6d32f40ba1b6428
Dawid Worhacz <worhacz.dawid@gmail.com>
2018-07-13 11:12:10 +0200

    koretka skryptow do obslugi sliderow Reddy'ego

--- merkuriusz/js/custom.js
+++ merkuriusz/js/custom.js
@@ -13,50 +13,59 @@ $(function(){
 	jQuery(".avialable-product-section .arrow-circle.right").click(function () {
 		jQuery(".avialable-product-section .bx-controls-direction .bx-next").trigger("click");
 	});
-
-	var owl = $("#owl-demo");
-	owl.owlCarousel({
-		items: 7,
-		itemsDesktop: [1184, 4],
-		itemsDesktopSmall: [976, 3],
-		pagination: false,
-	});
-	$(".next").click(function () {
-		owl.trigger('owl.next');
-	})
-	$(".prev").click(function () {
-		owl.trigger('owl.prev');
-	})
-
-	var owl = $("#product-slider");
-	owl.owlCarousel({
-		items: 3,
-		itemsDesktop: [1184, 3],
-		itemsDesktopSmall: [976, 2],
-		itemsTablet: [752, 1],
-		pagination: false,
-	});
-	$(".fifth-container .next").click(function () {
-		owl.trigger('owl.next');
-	})
-	$(".fifth-container .prev").click(function () {
-		owl.trigger('owl.prev');
-	})
 	
-	var owl = $("#category-item-slider");
-	owl.owlCarousel({
-		items: 4,
-		itemsDesktop: [1184, 3],
-		itemsDesktopSmall: [976, 2],
-		itemsTablet: [752, 1],
-		pagination: false,
-	});
-	$(".category-blocks .fifth-container .next").click(function () {
-		owl.trigger('owl.next');
-	})
-	$(".category-blocks .fifth-container .prev").click(function () {
-		owl.trigger('owl.prev');
-	})
+	(function(){
+		var owl = $("#owl-demo");
+		owl.owlCarousel({
+			items: 7,
+			itemsDesktop: [1184, 4],
+			itemsDesktopSmall: [976, 3],
+			pagination: false,
+		});
+		$(".next").click(function () {
+			owl.trigger('owl.next');
+		})
+		$(".prev").click(function () {
+			owl.trigger('owl.prev');
+		})
+		
+	})();
+
+	(function(){
+		var owl = $("#product-slider");
+		owl.owlCarousel({
+			items: 3,
+			itemsDesktop: [1184, 3],
+			itemsDesktopSmall: [976, 2],
+			itemsTablet: [752, 1],
+			pagination: false,
+		});
+		$(".fifth-container .next").click(function () {
+			owl.trigger('owl.next');
+		})
+		$(".fifth-container .prev").click(function () {
+			owl.trigger('owl.prev');
+		})
+		
+	})();
+	
+	(function(){
+		var owl = $("#category-item-slider");
+		owl.owlCarousel({
+			items: 4,
+			itemsDesktop: [1184, 3],
+			itemsDesktopSmall: [976, 2],
+			itemsTablet: [752, 1],
+			pagination: false,
+		});
+		$(".category-blocks .fifth-container .next").click(function () {
+			owl.trigger('owl.next');
+		})
+		$(".category-blocks .fifth-container .prev").click(function () {
+			owl.trigger('owl.prev');
+		})
+		
+	})();
 	
 	$(".vip-menu-block").click(function () {
 		if ($(".vip-menu").hasClass("active")) {
